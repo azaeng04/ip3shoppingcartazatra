7fbcc56bb89bdd9f7365e0a571be5a1040c6bf83
m04sengy <m04sengy@gmail.com>
2012-11-07 18:34:04 +0000

    Added new Data Structure. Import into myPhpAdmin. ShoppingCart.php modified according to the data structure.

--- Collectables/Controller/ShoppingCart.php
+++ Collectables/Controller/ShoppingCart.php
@@ -4,181 +4,179 @@ define('__ROOT__', dirname(__FILE__));
 require_once(__ROOT__.'\DBLayer\DBHandler.php'); 
 
 class ShoppingCart
-{
-            
-            private $DBHandler = null;
-            private $DBConnect = null;
-            private $inventory = array();
-            private $shoppingCart = array();
-            private $storeID = "";
+{      
+    private $DBHandler = null;
+    private $DBConnect = null;
+    private $inventory = array();
+    private $shoppingCart = array();
+    private $storeID = "";
 
 
-            function __construct()
+    function __construct()
+    {
+            $this->DBHandler = new DBHander();
+            $this->DBHandler->setDB('localhost', 'root', '', 'collectables');
+            $this->DBConnect = $this->DBHandler->connectToDB();
+            echo "<p>Hello DBDatabase connected successfully</p>";
+    }
+
+    function __destruct()
+    {
+            if(!$this->DBConnect->connect_error)
+                    $this->DBConnect->close();
+    }
+
+    function __wakeup()
+    {
+            $this->DBConnect = $this->DBHandler->connectToDB();
+    }
+
+    public function setStoreID($storeID)
+    {
+            if($this->storeID != $storeID)
             {
-                    $this->DBHandler = new DBHander();
-                    $this->DBHandler->setDB('localhost', 'root', '', 'Collectables');
-                    $this->DBConnect = $this->DBHandler->connectToDB();
-                    echo "<p>Hello DB</p>";
-            }
-
-            function __destruct()
-            {
-                    if(!$this->DBConnect->connect_error)
-                            $this->DBConnect->close();
-            }
-
-            function __wakeup()
-            {
-                    $this->DBConnect = $this->DBHandler->connectToDB();
-            }
-
-            public function setStoreID($storeID)
-            {
-                    if($this->storeID != $storeID)
-                    {
-                        $this->storeID = $storeID;
-                        $SQLString = "SELECT * FROM inventory WHERE storeID = '" . $this->storeID ."'";
-                        $QueryResult = @$this->DBConnect->query($SQLString);
-                        if($QueryResult === FALSE)
-                        {
-                            $this->storeID = "";
-                        }
-                        else
-                        {
-                            $this->inventory = array();
-                            while(($Row = $QueryResult->fetch_assoc()) !== NULL)
-                            {
-                                $this->inventory[$Row['productID']] = array();
-                                $this->inventory[$Row['productID']]['name'] = $Row['name'];
-                                $this->inventory[$Row['productID']]['description'] = $Row['description'];
-                                $this->inventory[$Row['productID']]['price'] = $Row['price'];
-                                $this->shoppingCart[$Row['productID']] = 0;
-                            }
-                        }
-                    }
-            }
-
-            public function getStoreInformation()
-            {
-                    $retval = FALSE;
-                    if($this->storeID != "")
-                    {
-                            $SQLString = "SELECT * FROM store_info WHERE storeID = '" . $this->storeID . "'";
-                            $QueryResult = @$this->DBConnect->query($SQLString);
-                            if($QueryResult !== FALSE)
-                            {
-                                            $retval = $QueryResult->fetch_assoc();
-                            }
-                    }
-                    return($retval);
-            }
-
-            public function getProductList()
-            {
-                    $retval = FALSE;
-                    $subtotal = 0;
-                    if(count($this->inventory) > 0)
-                    {
-                        echo"<table width= '100%'>\n";
-                        echo"<tr><th>Product</th><th>Description</th>".
-                        "<th>Price Each</th><th># in Cart</th>" .
-                        "<th>Total Price</th><th>&nbsp;</th></tr>\n";
-                        foreach($this->inventory as $ID => $Info)
-                        {
-                            echo "<tr><td>".
-                                    htmlentities($Info['name'])."</td>\n";
-                            echo "<td>".htmlentities($Info['description'])."</td>\n";
-                            printf("<td class= 'currency'>$%.2f</td>\n", $Info['price']);
-                            echo "<td class= 'currency'>".$this->shoppingCart[$ID]."</td>\n";
-                            printf("<td class= 'currency'>$%.2f</td>\n", $Info['price'] * $this->shoppingCart[$ID]);
-                            echo "<td><a href='" .
-                            $_SERVER['SCRIPT_NAME'] .
-                            "?PHPSESSID=" . session_id() .
-                            "&ItemToAdd=$ID'>Add " .
-                            " Item</a><br />\n";
-                            echo "<a href='" . $_SERVER['SCRIPT_NAME'].
-                            "?PHPSESSID=" . session_id() .
-                            "&ItemToRemove=$ID'>Remove " .
-                            " Item</a></td>\n";
-                            $subtotal += ($Info['price'] * $this->shoppingCart[$ID]);
-                        }
-                        echo "<tr><td colspan= '4'>Subtotal</td>\n";
-                        printf("<td class= 'currency'>$%.2f</td>\n", $subtotal);
-                        echo "<td><a href='" .
-                        $_SERVER['SCRIPT_NAME'] .						
-                        "?PHPSESSID=" . session_id() .
-                        "&EmptyCart=TRUE'>Empty ".
-                        " Cart</a></td></tr>\n";
-                        echo"</table>";
-                        $retval = TRUE;
-                    }
-                    return($retval);
-            }
-
-            public function showCart() 
-            {
-
-                        //testtest
-            }
-
-            private  function addItem()
-            {
-
-            }
-
-            private  function addOne()
-            {
-
-            }
-
-            private function removeItem()
-            {
-
-            }
-
-            private function emptyCart()
-            {
-
-            }
-
-            private function removeAll()
-            {
-
-            }
-
-            public function processUserInput()
-            {
-                    if(!empty($_GET['ItemToAdd']))
-                            $this->addItem();
-                    if(!empty($_GET['AddOne']))
-                            $this->addOne();
-                    if(!empty($_GET['ItemToRemove']))
-                            $this->removeItem();
-                    if(!empty($_GET['EmptyCart']))
-                            $this->emptyCart();
-                    if(!empty($_GET['RemoveAll']))
-                            $this->removeAll();
-            }
-
-            public function getProductInfo($prodID) 
-            {
-                    $SQLstring = "SELECT * FROM inventory WHERE productID = '" . $prodID . "'";
-                    $QueryResult = @$this->DBConnect->query($SQLstring);
-                    if($QueryResult === FALSE)
-                        exit("<p>Error Obtaining Product Info!</p>");
-                    else 
-                    {
-                        $Row = $QueryResult->fetch_assoc();
-                        if($Row !== NULL) 
-                        {
-                                return($Row);
-                        }
-                    }			
-            }
-
-            public function checkout()
-            {			
-                    echo "<p><strong>Your order has been recorded.</strong></p>\n";
+                $this->storeID = $storeID;
+                $SQLString = "SELECT * FROM product WHERE storeID = '" . $this->storeID ."'";
+                $QueryResult = @$this->DBConnect->query($SQLString);
+                $this->populateInventory($QueryResult);
             }
     }
+
+    private function populateInventory($QueryResult)
+    {
+        if($QueryResult === FALSE)
+        {
+            $this->storeID = "";
+        }
+        else
+        {
+            $this->inventory = array();
+            while(($Row = $QueryResult->fetch_assoc()) !== NULL)
+            {
+                $this->inventory[$Row['prodID']] = array();
+                $this->inventory[$Row['prodID']]['prodName'] = $Row['prodName'];
+                $this->inventory[$Row['prodID']]['prodDesc'] = $Row['prodDesc'];
+                $this->inventory[$Row['prodID']]['prodPrice'] = $Row['prodPrice'];
+                $this->shoppingCart[$Row['prodID']] = 0;
+            }
+        }
+    }
+
+    public function getStoreInformation()
+    {
+            $retval = FALSE;
+            if($this->storeID != "")
+            {
+                    $SQLString = "SELECT * FROM storeinfo WHERE storeID = '" . $this->storeID . "'";
+                    $QueryResult = @$this->DBConnect->query($SQLString);
+                    if($QueryResult !== FALSE)
+                    {
+                        $retval = $QueryResult->fetch_assoc();
+                    }
+            }
+            return($retval);
+    }
+
+    public function getProductList()
+    {
+            $retval = FALSE;
+            $subtotal = 0;
+            if(count($this->inventory) > 0)
+            {
+                echo"<table width= '100%'>\n";
+                echo"<tr><th>Product Name</th><th>Product Description</th>".
+                        "<th>Price Each</th><th>Quantity in Cart</th>" .
+                        "<th>Total Price</th><th>&nbsp;</th></tr>\n";
+                $this->populateTableContent($subtotal);
+                echo "<tr><td colspan= '4' align='right' >Subtotal</td>\n";
+                printf("<td >$%.2f</td>\n", $subtotal);
+                echo "<td ><a href='" . $_SERVER['SCRIPT_NAME'] . "?PHPSESSID=" . session_id() .
+                            "&EmptyCart=TRUE'>Empty ". " Cart</a></td></tr>\n";
+                echo"</table>";
+                $retval = TRUE;
+            }
+            return($retval);
+    }
+
+    private function populateTableContent(&$subtotal)
+    {
+        foreach($this->inventory as $ID => $Info)
+        {
+            echo "<tr><td >". htmlentities($Info['prodName'])."</td>\n";
+            echo "<td >".htmlentities($Info['prodDesc'])."</td>\n";
+            printf("<td >$%.2f</td>\n", $Info['prodPrice']);
+            echo "<td >".$this->shoppingCart[$ID]."</td>\n";
+            printf("<td >$%.2f</td>\n", $Info['prodPrice'] * $this->shoppingCart[$ID]);
+            echo "<td ><a href='" . $_SERVER['SCRIPT_NAME'] . "?PHPSESSID=" . session_id() . 
+                         "&ItemToAdd=$ID'>Add " . " Item</a><br />\n";
+            echo "<a href='" . $_SERVER['SCRIPT_NAME']. "?PHPSESSID=" . session_id() .
+                         "&ItemToRemove=$ID'>Remove " . " Item</a></td>\n";
+            $subtotal += ($Info['prodPrice'] * $this->shoppingCart[$ID]);
+        }
+        return $subtotal;
+    }
+    public function showCart() 
+    {
+    }
+
+    private  function addItem()
+    {
+
+    }
+
+    private  function addOne()
+    {
+
+    }
+
+    private function removeItem()
+    {
+
+    }
+
+    private function emptyCart()
+    {
+
+    }
+
+    private function removeAll()
+    {
+
+    }
+
+    public function processUserInput()
+    {
+            if(!empty($_GET['ItemToAdd']))
+                    $this->addItem();
+            if(!empty($_GET['AddOne']))
+                    $this->addOne();
+            if(!empty($_GET['ItemToRemove']))
+                    $this->removeItem();
+            if(!empty($_GET['EmptyCart']))
+                    $this->emptyCart();
+            if(!empty($_GET['RemoveAll']))
+                    $this->removeAll();
+    }
+
+    public function getProductInfo($prodID) 
+    {
+            $SQLstring = "SELECT * FROM product WHERE prodID = '" . $prodID . "'";
+            $QueryResult = @$this->DBConnect->query($SQLstring);
+            if($QueryResult === FALSE)
+                exit("<p>Error Obtaining Product Info!</p>");
+            else 
+            {
+                $Row = $QueryResult->fetch_assoc();
+                if($Row !== NULL) 
+                {
+                        return($Row);
+                }
+            }			
+    }
+
+    public function checkout()
+    {			
+            echo "<p><strong>Your order has been recorded.</strong></p>\n";
+    }
+}
 ?>
--- Collectables/DBLayer/DBHandler.php
+++ Collectables/DBLayer/DBHandler.php
@@ -13,14 +13,12 @@
  */
 class DBHander 
 {
-    
     var $db_port = null;
     var $db_user = null;
     var $db_pass = null;
     var $db_name = null;
     var $errorMsgs = null;
-    
-    
+        
     function setDB($db_port,$db_user,$db_pass,$db_name)
     {
         $this->db_port = $db_port;
@@ -31,13 +29,13 @@ class DBHander
     
     function connectToDB()
     {
-                        $this->errorMsgs = array();
-			$dbcnx = new mysqli($this->db_port, $this->db_user,$this->db_pass, $this->db_name);
-			if($dbcnx->connect_errno)
-                        {
-                                $this->errorMsgs[] = "Unable to connect to the database server. Error code " . $dbcnx->connect_errno . ": " . $dbcnx->connect_error;
-                        }
-                        return $dbcnx;
+        $this->errorMsgs = array();
+        $dbcnx = new mysqli($this->db_port, $this->db_user,$this->db_pass, $this->db_name);
+        if($dbcnx->connect_errno)
+        {
+                $this->errorMsgs[] = "Unable to connect to the database server. Error code " . $dbcnx->connect_errno . ": " . $dbcnx->connect_error;
+        }
+        return $dbcnx;
      }
      
      function clearErrorMsgs()
@@ -71,7 +69,5 @@ class DBHander
      {
          return ($dbcnx->close());
      }
-
 }
-
 ?>
