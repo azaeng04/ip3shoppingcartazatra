f4e3c6d64d0ff5e458b3e8552ab8b373fa4c22b0
m04sengy@gmail.com <m04sengy@gmail.com@1e284473-9333-977e-9cfb-a92943901752>
2012-11-10 14:16:27 +0000

    Check out shopping cart working.

--- Collectables/DBLayer/DBHander.php
+++ Collectables/DBLayer/DBHander.php
@@ -13,12 +13,17 @@
  */
 class DBHander 
 {
-    var $db_port = null;
-    var $db_user = null;
-    var $db_pass = null;
-    var $db_name = null;
-    var $errorMsgs = null;
-        
+    var $db_conn = NULL;
+    var $db_port = NULL;
+    var $db_user = NULL;
+    var $db_pass = NULL;
+    var $db_name = NULL;
+    var $errorMsgs = NULL;
+    
+    function __construct() {
+        $this->setDB('localhost', 'root', '', 'collectables');
+    }
+
     function setDB($db_port,$db_user,$db_pass,$db_name)
     {
         $this->db_port = $db_port;
@@ -30,22 +35,117 @@ class DBHander
     function connectToDB()
     {
         $this->errorMsgs = array();
-        $dbcnx = new mysqli($this->db_port, $this->db_user,$this->db_pass, $this->db_name);
-        if($dbcnx->connect_errno)
+        try 
         {
+            $dbcnx = new mysqli($this->db_port, $this->db_user, $this->db_pass, $this->db_name);
+            if ($dbcnx->connect_errno)         {
                 $this->errorMsgs[] = "Unable to connect to the database server. Error code " . $dbcnx->connect_errno . ": " . $dbcnx->connect_error;
+            }         else         {
+                $this->db_conn = $dbcnx;
+                return $dbcnx;
+            }
+        } 
+        catch (Exception $exc) 
+        {
+            echo $exc->getMessage();
         }
-        return $dbcnx;
-     }
+         }
      
-     function clearErrorMsgs()
-     {
+    function clearErrorMsgs()
+    {
          foreach ($this->errorMsgs as $value)
          {
              unset($value);
          }
      }
      
+     public function getMaxID($table, $column)
+     {            
+        $count = "SELECT COUNT(*) FROM $table;";
+        $query = "SELECT MAX($column) FROM $table;";            
+        $maxID = 1;
+
+        try 
+        {
+            $res = $this->db_conn->query($count);
+            $row = $res->fetch_row();
+            if ($row[0] > 0) 
+            {
+                $res = $this->db_conn->query($query);
+
+                $row = $res->fetch_row();
+                $maxID = $row[0];
+                return ($maxID + 1);
+            } 
+            else 
+            {
+                return $maxID;
+            }
+        } 
+        catch (Exception $exc) 
+        {
+            echo $exc->getMessage();
+        }
+     }
+
+     private function insertIntoOrders($orderID)
+     {
+         $insert = "INSERT INTO ORDERS VALUES($orderID, CURDATE())";
+         
+         $this->insertUpdateDelete($insert);
+     }
+
+     public function insertIntoOrderLine($shoppingCartArray)
+     {
+         try 
+         {
+            $maxOrderID = $this->getMaxID("orders", "orderID");
+            $this->insertIntoOrders($maxOrderID);
+            foreach ($shoppingCartArray as $productID => $quantity) 
+            {
+                $maxOrdlineID = $this->getMaxID("orderline", "orderLineID");
+                $insertOrder = "INSERT INTO orderline VALUES($maxOrdlineID, $maxOrderID, $productID, $quantity)";
+
+                $this->insertUpdateDelete($insertOrder);
+                echo "<p>Orderline ID: $maxOrdlineID, Order ID: $maxOrderID, Product ID: $productID Quantity: $quantity </p>";
+            }
+            $this->updateInStock($shoppingCartArray);            
+         }
+         catch (Exception $exc) 
+         {
+             echo $exc->getMessage();
+         }
+     }
+     
+     private function updateInStock($shoppingCartArray)
+     {
+         try 
+         {  
+             foreach ($shoppingCartArray as $key => $value) 
+             {
+                 $updateInStock = "UPDATE product SET inStock = inStock - $value WHERE prodID = $key";
+                 $this->insertUpdateDelete($updateInStock);
+             }
+         } 
+         catch (Exception $exc) 
+         {
+             echo $exc->getMessage();
+         }
+     }
+     
+     private function insertUpdateDelete($query)
+     {
+         try 
+         {
+             $this->db_conn->query($query);             
+         } 
+         catch (Exception $exc) 
+         {
+             echo $exc->getMessage();
+         }
+     }
+
+
      function displayErrorMsgs()
      {
          foreach ($this->errorMsgs as $value)
